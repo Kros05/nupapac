ca5d8b28aa7b4c34e6583077fe2b702bbd1d95dc
Stanislav Krasovskyi <stanislav.krasovskyi@gmail.com>
2018-09-09 15:11:11 +0300

    Console quiz

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello world!");
+var readlineSync = require('readline-sync');
+
+var name = readlineSync.question('What is your name? ');
+
+var patron = 'Sun';
+
+console.log(name + ', your planetary patron is ' + patron);
